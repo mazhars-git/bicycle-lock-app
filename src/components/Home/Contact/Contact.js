4983a9df60a591c784b2912f27e38b664e803dcc
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-08-26 11:03:09 +0600

    conatac info design

--- src/components/Home/Contact/Contact.js
+++ src/components/Home/Contact/Contact.js
@@ -1,3 +1,6 @@
+import { faEnvelope, faUser } from '@fortawesome/free-regular-svg-icons';
+import { faPen, faPenSquare } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
 const Contact = () => {
@@ -5,32 +8,41 @@ const Contact = () => {
         <section className="container py-4">
             <div className="row">
                 <div className="col-md-6">
-                    <h5>Contact Us</h5>
-                    <h3>Get &copy; Catch Anytime From Anywhere <br />We Are Available Anytime.</h3>
+                    <h5 className='semi-heading'>Contact Us</h5>
+                    <h3 className='small-heading'>Get & Catch Anytime From Anywhere <br />We Are Available Anytime.</h3>
                     <h6>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla odio corrupti qui, placeat iure sit.</h6>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam mollitia ullam repellat libero provident minima necessitatibus ratione eum aspernatur vitae.</p>
                 </div>
                 <div className="col-md-6">
-                    <h2>Get A Free Quote</h2>
+                    <h2 className="small-heading">Get A Free Quote</h2>
                     <form>
-                        <div className="input-group form-group">
+                        <div className="input-group form-group">                            
+                            <div className="input-group-prepend">
+                                <input type="text" className="form-control" placeholder="Full Name" />
+                                <span><FontAwesomeIcon icon={faUser} /></span>
+                            </div>
+                        </div>
+                        <div className="input-group form-group">                            
+                            <div className="input-group-prepend">
+                                <input type="email" className="form-control" placeholder="Email Address" />
+                                <span><FontAwesomeIcon icon={faEnvelope} /></span>
+                            </div>
+                        </div>
+                        <div className="input-group form-group">                            
+                            <div className="input-group-prepend">
+                                <input type="text" className="form-control" placeholder="Enter Message" />
+                                <span><FontAwesomeIcon icon={faPen} /></span>
+                            </div>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Get In Touch</button>
+                       
+                        {/* <div className="input-group form-group">
                             <div className="input-group-prepend">
                                 <span><i className="fas fa-user"></i></span>
                             </div>
                             <input type="text" className="form-control" placeholder="username" />
-                        </div>
-                        <div className="input-group form-group">
-                            <div className="input-group-prepend">
-                                <span><i className="fas fa-user"></i></span>
-                            </div>
-                            <input type="text" className="form-control" placeholder="username" />
-                        </div>
-                        <div className="input-group form-group">
-                            <div className="input-group-prepend">
-                                <span><i className="fas fa-user"></i></span>
-                            </div>
-                            <input type="text" className="form-control" placeholder="username" />
-                        </div>
+                        </div> */}
                     </form>
 
                 </div>
