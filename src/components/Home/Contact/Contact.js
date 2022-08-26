de948b8a174b0c05924eeebe1266cbd810d3141d
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-08-26 20:26:01 +0600

    contact page done

--- src/components/Home/Contact/Contact.js
+++ src/components/Home/Contact/Contact.js
@@ -6,46 +6,43 @@ import "../../CSS/Contact.css";
 
 const Contact = () => {
     return (
-        <section className="container py-4">
-            <div className="row">
+        <section className="container py-5">
+            <div className="row py-3">
                 <div className="col-md-7 align-self-center">
-                    <h5 className='semi-heading'>Contact Us</h5>
-                    <h3 className='small-heading'>Get & Catch Anytime From Anywhere <br />We Are Available Anytime.</h3>
-                    <h6>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla odio corrupti qui, placeat iure sit.</h6>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam mollitia ullam repellat libero provident minima necessitatibus ratione eum aspernatur vitae.</p>
+                    <div className="contact-info">
+                        <h5 className='semi-heading'>Contact Us</h5>
+                        <h3 className='small-heading py-2'>Get & Catch Anytime From Anywhere <br />We Are Available Anytime.</h3>
+                        <h6>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla odio corrupti qui, placeat iure sit.</h6>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam mollitia ullam repellat libero provident minima necessitatibus ratione eum aspernatur vitae.</p>
+                    </div>
                 </div>
-                <div className="col-md-5 contact-form">
-                    <h2 className="small-heading pb-3">Get A Free Quote</h2>
-                    <form>
-                        <div className="input-group form-group">                            
-                            <div className="input-group">
-                                <input type="text" className="form-control" placeholder="Full Name" />
-                                <span><FontAwesomeIcon icon={faUser} /></span>
-                            </div>
-                        </div>
-                        <div className="input-group form-group">                            
-                            <div className="input-group">
-                                <input type="email" className="form-control" placeholder="Email Address" />
-                                <span><FontAwesomeIcon icon={faEnvelope} /></span>
-                            </div>
-                        </div>
-                        <div className="input-group form-group">                            
-                            <div className="input-group">
-                                <textarea rows="4" cols="50" className="form-control" placeholder="Enter Message" />
-                                <span><FontAwesomeIcon icon={faPen} /></span>
-                            </div>
-                        </div>
 
-                        <button type="submit" class="btn btn-brand" style={{backgroundColor: 'orange'}}>Get In Touch</button>
-                       
-                        {/* <div className="input-group form-group">
-                            <div className="input-group-prepend">
-                                <span><i className="fas fa-user"></i></span>
+                <div className="col-md-5">
+                    <div className="contact-form">
+                        <h2 className="small-heading pb-3">Get A Free Quote</h2>
+                        <form>
+                            <div className="input-group form-group">                            
+                                <div className="input-group">
+                                    <input type="text" className="form-control" placeholder="Full Name" />
+                                    <span className='icon'><FontAwesomeIcon icon={faUser} /></span>
+                                </div>
                             </div>
-                            <input type="text" className="form-control" placeholder="username" />
-                        </div> */}
-                    </form>
+                            <div className="input-group form-group">                            
+                                <div className="input-group">
+                                    <input type="email" className="form-control" placeholder="Email Address" />
+                                    <span className='icon'><FontAwesomeIcon icon={faEnvelope} /></span>
+                                </div>
+                            </div>
+                            <div className="input-group form-group">                            
+                                <div className="input-group">
+                                    <textarea rows="4" cols="50" className="form-control" placeholder="Enter Message" />
+                                    <span className='icon'><FontAwesomeIcon icon={faPen} /></span>
+                                </div>
+                            </div>
 
+                            <button type="submit" class="btn btn-brand" style={{backgroundColor: 'orange'}}>Get In Touch</button>
+                        </form>
+                    </div>
                 </div>
             </div>
             
