687cd71a8b1e5576a1722e5caad372d36f6c15b0
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-08-27 20:27:46 +0600

    service section update

--- src/components/Home/Services/Services.js
+++ src/components/Home/Services/Services.js
@@ -1,31 +1,38 @@
 import React from 'react';
 import LockBg from "../../../images/image-2.png";
+import '../../CSS/Services.css';
 
 const Services = () => {
     return (
         <section className="container py-5">
             <div className="row">
-                <div className="col-md-6">
+                <div className="col-md-6 align-self-center">
                     <img className="img-fluid" src={LockBg} alt="" />
                 </div>
                 <div className="col-md-6">
-                    <h6>Why Choose Us</h6>
-                    <h2>We Provide Smart &amp; <br />Digital Services.</h2>
+                    <h6 className="semi-heading">Why Choose Us</h6>
+                    <h2 className='small-heading'>We Provide Smart &amp; <br />Digital Services.</h2>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque dolorem, maiores voluptas voluptatum consectetur dolor iure maxime inventore aliquid illum?</p>
 
-                    <div>
-                        <h4>Bluetooth Connection</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem id perspiciatis obcaecati est quae consequuntur.</p>
+                    <div className="service-item-box">
+                        <div className="service-item">
+                            <h4>Bluetooth Connection</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem id  perspiciatis obcaecati est quae consequuntur.</p>
+                        </div>                        
                     </div>
 
-                    <div>
-                        <h4>USB Rechargeable</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem id perspiciatis obcaecati est quae consequuntur.</p>
+                    <div className="service-item-box">
+                        <div className="service-item">
+                            <h4>USB Rechargeable</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem id perspiciatis obcaecati est quae consequuntur.</p>
+                        </div>
                     </div>
 
-                    <div>
-                        <h4>Ultra-Long Standby</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem id perspiciatis obcaecati est quae consequuntur.</p>
+                    <div className="service-item-box">
+                        <div className="service-item">
+                            <h4>Ultra-Long Standby</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem id perspiciatis obcaecati est quae consequuntur.</p>
+                        </div>
                     </div>
                 </div>
             </div>
