87735afd2077f79be184b264d1e871bf0cdd9795
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-08-27 13:00:05 +0600

    menu update

--- src/components/Home/MainMenu/MainMenu.js
+++ src/components/Home/MainMenu/MainMenu.js
@@ -1,31 +1,32 @@
 import React from 'react';
 import Logo from '../../../images/logo.png';
+import '../../CSS/MainMenu.css';
 
 const MainMenu = () => {
     return (
         <div>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <nav class="navbar navbar-expand-lg navbar-light bg-none">
                 <div class="container-fluid">
                     <a class="navbar-brand" href="#"><img src={Logo} alt="" /></a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
+                    <div class="collapse navbar-collapse nav-menu-item" id="navbarNav">
                         <ul class="navbar-nav ml-auto">
                             <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                <a class="nav-link active" aria-current="page" href="#">Home</a>
                             </li>
                             <li class="nav-item">
-                            <a class="nav-link" href="#">Services</a>
+                                <a class="nav-link" href="#">Services</a>
                             </li>
                             <li class="nav-item">
-                            <a class="nav-link" href="#">Products</a>
+                                <a class="nav-link" href="#">Products</a>
                             </li>
                             <li class="nav-item">
-                            <a class="nav-link" href="#">Contact</a>
+                                <a class="nav-link" href="#">Contact</a>
                             </li>
                         </ul>
-                        <button className="btn btn-primary">Learn More</button>
+                        <button className="btn btn-brand menu-btn">Shop Now</button>
                     </div>
 
                 </div>
