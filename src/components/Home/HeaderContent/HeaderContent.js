2e8a2d0cffac61ff5ea1fbdfc8077a895142efee
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-08-27 16:57:55 +0600

    header update

--- src/components/Home/HeaderContent/HeaderContent.js
+++ src/components/Home/HeaderContent/HeaderContent.js
@@ -5,10 +5,10 @@ const HeaderContent = () => {
     return (
         <section className="container">
             <div className="row">
-                <div className="col-md-6 py-5">
-                    <h1>Smart <br /> Bicycle Lock</h1>
+                <div className="col-md-6 py-5 align-self-center">
+                    <h1 className="big-heading">Smart <br /> Bicycle Lock</h1>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit voluptatum facilis delectus quas aut unde laboriosam iusto? Eveniet, vel maxime!</p>
-                    <button className="btn btn-primary">Shop Now</button>
+                    <button className="btn btn-brand">Shop Now</button>
                 </div>
                 <div className="col-md-6">
                     <img className="img-fluid" src={Lock} alt="" />
